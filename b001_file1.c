baed494fb1f0ab03e9b979fc10af2cbbe5fb94dd
刘百涛 <liubt@landicorp.com>
2019-06-29 17:20:50 +0800

    modify b001_file1.c and main.c

--- b001_file1.c
+++ b001_file1.c
@@ -1,3 +1,7 @@
 void b001_func(void)
 {
+	//this is master modify
+	int i;
+	for(i=0;i<100;i++)
+		printf("i=%d\n",i);
 }
---
+++ main.c
@@ -0,0 +1,7 @@
+extern void b001_func(void);
+
+int main(int argc,char **argv)
+{
+	b001_func();
+	return 0;
+}
